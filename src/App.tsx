c92133aedc158ff150f369ff17ed61b1bf47da43
taishikato <taishi.k0903@gmail.com>
2022-08-04 11:54:52 -0700

    create Body component

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,7 @@
 import { useDnDSort } from "./hooks/useDnDSort";
 import { css } from "@emotion/css";
 import Header from "./Header/Header";
+import Body from "./Body/Body";
 
 const itemList: string[] = Array.from(Array(100)).map((_, index) => {
   return `Row header ${index}`;
@@ -25,130 +26,7 @@ function App() {
         `}
       >
         <Header />
-        <tbody>
-          {results.map((r) => {
-            return (
-              <tr
-                key={r.key}
-                {...r.events}
-                className={css`
-                  cursor: grab;
-                  position: relative;
-                  touch-action: none;
-                `}
-              >
-                <th
-                  className={css`
-                    min-width: 130px;
-                    vertical-align: middle;
-                    padding: 10px 10px 10px 0;
-                    text-align: left;
-                    font-weight: 600;
-                    user-select: none;
-                    box-sizing: border-box;
-                    border-bottom: 1px solid rgb(203 213 225);
-                    position: sticky;
-                    left: 0;
-                    box-shadow: 3px 0px 2px rgba(0, 0, 0, 0.1);
-                    background-color: #ffffff;
-                  `}
-                >
-                  {r.value}
-                </th>
-                <td
-                  className={css`
-                    padding: 10px;
-                    vertical-align: middle;
-                    background-color: #ffffff;
-                    padding: 10px;
-                    text-align: center;
-                    font-weight: 600;
-                    user-select: none;
-                    box-sizing: border-box;
-                    border-bottom: 1px solid rgb(203 213 225);
-                    &:not(:last-child) {
-                      border-right: 1px solid rgb(203 213 225);
-                    }
-                  `}
-                >
-                  td 1
-                </td>
-                <td
-                  className={css`
-                    padding: 10px;
-                    vertical-align: middle;
-                    background-color: #ffffff;
-                    padding: 10px;
-                    text-align: center;
-                    font-weight: 600;
-                    user-select: none;
-                    box-sizing: border-box;
-                    border-bottom: 1px solid rgb(203 213 225);
-                    &:not(:last-child) {
-                      border-right: 1px solid rgb(203 213 225);
-                    }
-                  `}
-                >
-                  td 2
-                </td>
-                <td
-                  className={css`
-                    padding: 10px;
-                    vertical-align: middle;
-                    background-color: #ffffff;
-                    padding: 10px;
-                    text-align: center;
-                    font-weight: 600;
-                    user-select: none;
-                    box-sizing: border-box;
-                    border-bottom: 1px solid rgb(203 213 225);
-                    &:not(:last-child) {
-                      border-right: 1px solid rgb(203 213 225);
-                    }
-                  `}
-                >
-                  td 3
-                </td>
-                <td
-                  className={css`
-                    padding: 10px;
-                    vertical-align: middle;
-                    background-color: #ffffff;
-                    padding: 10px;
-                    text-align: center;
-                    font-weight: 600;
-                    user-select: none;
-                    box-sizing: border-box;
-                    border-bottom: 1px solid rgb(203 213 225);
-                    &:not(:last-child) {
-                      border-right: 1px solid rgb(203 213 225);
-                    }
-                  `}
-                >
-                  td 4
-                </td>
-                <td
-                  className={css`
-                    padding: 10px;
-                    vertical-align: middle;
-                    background-color: #ffffff;
-                    padding: 10px;
-                    text-align: center;
-                    font-weight: 600;
-                    user-select: none;
-                    box-sizing: border-box;
-                    border-bottom: 1px solid rgb(203 213 225);
-                    &:not(:last-child) {
-                      border-right: 1px solid rgb(203 213 225);
-                    }
-                  `}
-                >
-                  td 5
-                </td>
-              </tr>
-            );
-          })}
-        </tbody>
+        <Body items={results} />
       </table>
     </div>
   );
