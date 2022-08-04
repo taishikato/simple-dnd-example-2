ecee029d82afdc4bf31ffdc6c0a2ced15beee3b9
taishikato <taishi.k0903@gmail.com>
2022-08-03 20:03:41 -0700

    adda table

--- src/App.tsx
+++ src/App.tsx
@@ -37,27 +37,49 @@ const imageStyle: Style<HTMLImageElement> = {
 };
 
 // 並び替えしたい画像URLの配列
-const imageList: string[] = [
-  "https://images.pexels.com/photos/12906892/pexels-photo-12906892.jpeg",
-  "https://images.pexels.com/photos/12638731/pexels-photo-12638731.jpeg",
-  "https://images.pexels.com/photos/5720781/pexels-photo-5720781.jpeg",
-  /* ... */
-];
+// const imageList: string[] = [
+//   "https://images.pexels.com/photos/12906892/pexels-photo-12906892.jpeg",
+//   "https://images.pexels.com/photos/12638731/pexels-photo-12638731.jpeg",
+//   "https://images.pexels.com/photos/5720781/pexels-photo-5720781.jpeg",
+//   /* ... */
+// ];
+
+const itemList: string[] = ["Row header 1", "Row header 2", "Row header "];
 
 // ドラッグ＆ドロップ並び替えサンプルのコンポーネント
 function App() {
-  const results = useDnDSort(imageList);
+  const results = useDnDSort(itemList);
 
   return (
-    <div style={bodyStyle}>
-      <div style={containerStyle}>
-        {results.map((item) => (
-          <div key={item.key} style={imageCardStyle} {...item.events}>
-            <img src={item.value} alt="ソート可能な画像" style={imageStyle} />
-          </div>
-        ))}
-      </div>
-    </div>
+    // <div style={bodyStyle}>
+    //   <div style={containerStyle}>
+    //     {results.map((item) => (
+    //       <div key={item.key} style={imageCardStyle} {...item.events}>
+    //         <img src={item.value} alt="ソート可能な画像" style={imageStyle} />
+    //       </div>
+    //     ))}
+    //   </div>
+    // </div>
+    <table>
+      <thead>
+        <tr>
+          <th>Th</th>
+          <th>Th2</th>
+          <th>Th3</th>
+        </tr>
+      </thead>
+      <tbody>
+        {results.map((r) => {
+          return (
+            <tr key={r.key} {...r.events} style={{ cursor: "grab" }}>
+              <th style={{ padding: "10px" }}>{r.value}</th>
+              <td>td 1</td>
+              <td>td 2</td>
+            </tr>
+          );
+        })}
+      </tbody>
+    </table>
   );
 }
 
