398aa782a7c8ca2180978fa882e6083ea26ddaba
taishikato <taishi.k0903@gmail.com>
2022-08-03 20:18:28 -0700

    add some CSSs

--- src/App.tsx
+++ src/App.tsx
@@ -1,66 +1,19 @@
 import { useDnDSort } from "./useDnDSort";
+import { css } from "@emotion/css";
 
-type Style<T extends HTMLElement> = React.HTMLAttributes<T>["style"];
-
-const bodyStyle: Style<HTMLDivElement> = {
-  height: "100vh",
-  display: "flex",
-  overflow: "hidden",
-  alignItems: "center",
-  justifyContent: "center",
-};
-
-const containerStyle: Style<HTMLDivElement> = {
-  display: "flex",
-  flexWrap: "wrap",
-  justifyContent: "space-between",
-  width: "100%",
-  maxWidth: "350px",
-  maxHeight: "500px",
-};
-
-const imageCardStyle: Style<HTMLDivElement> = {
-  cursor: "grab",
-  userSelect: "none",
-  width: "100px",
-  height: "130px",
-  overflow: "hidden",
-  borderRadius: "5px",
-  margin: 3,
-};
-
-const imageStyle: Style<HTMLImageElement> = {
-  pointerEvents: "none",
-  objectFit: "cover",
-  width: "100%",
-  height: "100%",
-};
-
-// 並び替えしたい画像URLの配列
-// const imageList: string[] = [
-//   "https://images.pexels.com/photos/12906892/pexels-photo-12906892.jpeg",
-//   "https://images.pexels.com/photos/12638731/pexels-photo-12638731.jpeg",
-//   "https://images.pexels.com/photos/5720781/pexels-photo-5720781.jpeg",
-//   /* ... */
-// ];
-
-const itemList: string[] = ["Row header 1", "Row header 2", "Row header "];
+const itemList: string[] = ["Row header 1", "Row header 2", "Row header 3"];
 
 // ドラッグ＆ドロップ並び替えサンプルのコンポーネント
 function App() {
   const results = useDnDSort(itemList);
 
   return (
-    // <div style={bodyStyle}>
-    //   <div style={containerStyle}>
-    //     {results.map((item) => (
-    //       <div key={item.key} style={imageCardStyle} {...item.events}>
-    //         <img src={item.value} alt="ソート可能な画像" style={imageStyle} />
-    //       </div>
-    //     ))}
-    //   </div>
-    // </div>
-    <table>
+    <table
+      className={css`
+        border-collapse: separate;
+        border-spacing: 0px 0px;
+      `}
+    >
       <thead>
         <tr>
           <th>Th</th>
@@ -74,11 +27,42 @@ function App() {
             <tr
               key={r.key}
               {...r.events}
-              style={{ cursor: "grab", touchAction: "none" }}
+              className={css`
+                cursor: grab;
+                touch-action: none;
+              `}
             >
-              <th style={{ padding: "10px" }}>{r.value}</th>
-              <td>td 1</td>
-              <td>td 2</td>
+              <th
+                className={css`
+                  padding: 10px;
+                  border-right: 1px solid rgb(203 213 225);
+                  border-bottom: 1px solid rgb(203 213 225);
+                `}
+              >
+                {r.value}
+              </th>
+              <td
+                className={css`
+                  padding: 10px;
+                  border-bottom: 1px solid rgb(203 213 225);
+                  &:not(:last-child) {
+                    border-right: 1px solid rgb(203 213 225);
+                  }
+                `}
+              >
+                td 1
+              </td>
+              <td
+                className={css`
+                  padding: 10px;
+                  border-bottom: 1px solid rgb(203 213 225);
+                  &:not(:last-child) {
+                    border-right: 1px solid rgb(203 213 225);
+                  }
+                `}
+              >
+                td 2
+              </td>
             </tr>
           );
         })}
