f489516c163af72c44bf9959e4857010ed9e94c7
taishikato <taishi.k0903@gmail.com>
2022-08-03 20:22:14 -0700

    add CSS to TH in a header

--- src/App.tsx
+++ src/App.tsx
@@ -16,7 +16,22 @@ function App() {
     >
       <thead>
         <tr>
-          <th>Th</th>
+          <th
+            className={css`
+              min-width: 150px;
+              vertical-align: middle;
+              padding: 10px 10px 10px 0;
+              text-align: left;
+              border-bottom: 1px solid #0b1424;
+              position: sticky;
+              top: 0px;
+              left: 0;
+              z-index: 20;
+              background-color: #ffffff;
+            `}
+          >
+            Th
+          </th>
           <th>Th2</th>
           <th>Th3</th>
         </tr>
