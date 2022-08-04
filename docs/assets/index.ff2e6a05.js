ad0b8c0b0222cd35e2f1132bbe9ce45ea8eb862c
taishikato <taishi.k0903@gmail.com>
2022-08-03 20:28:17 -0700

    update a demo

--- docs/assets/index.ff2e6a05.js
+++ docs/assets/index.ff2e6a05.js
@@ -71,17 +71,40 @@ Error generating stack: `+i.message+`
                 cursor: grab;
                 touch-action: none;
               `,children:[Be("th",{className:un`
-                  padding: 10px;
-                  border-right: 1px solid rgb(203 213 225);
+                  min-width: 130px;
+                  vertical-align: middle;
+                  padding: 10px 10px 10px 0;
+                  text-align: left;
+                  font-weight: 600;
+                  user-select: none;
+                  box-sizing: border-box;
                   border-bottom: 1px solid rgb(203 213 225);
+                  position: sticky;
+                  left: 0;
+                  box-shadow: 3px 0px 2px rgba(0, 0, 0, 0.1);
+                  background-color: #ffffff;
                 `,children:n.value}),Be("td",{className:un`
                   padding: 10px;
+                  vertical-align: middle;
+                  background-color: #ffffff;
+                  padding: 10px;
+                  text-align: center;
+                  font-weight: 600;
+                  user-select: none;
+                  box-sizing: border-box;
                   border-bottom: 1px solid rgb(203 213 225);
                   &:not(:last-child) {
                     border-right: 1px solid rgb(203 213 225);
                   }
                 `,children:"td 1"}),Be("td",{className:un`
                   padding: 10px;
+                  vertical-align: middle;
+                  background-color: #ffffff;
+                  padding: 10px;
+                  text-align: center;
+                  font-weight: 600;
+                  user-select: none;
+                  box-sizing: border-box;
                   border-bottom: 1px solid rgb(203 213 225);
                   &:not(:last-child) {
                     border-right: 1px solid rgb(203 213 225);
