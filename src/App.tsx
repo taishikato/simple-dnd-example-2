1a80f1d1eed5a9672695d29e39cee0afc654cd39
taishikato <taishi.k0903@gmail.com>
2022-08-04 11:41:46 -0700

    add a container div

--- src/App.tsx
+++ src/App.tsx
@@ -10,222 +10,230 @@ function App() {
   const results = useDnDSort(itemList);
 
   return (
-    <table
+    <div
       className={css`
-        border-collapse: separate;
-        border-spacing: 0px 0px;
+        width: 600px;
+        margin: 0 auto;
       `}
     >
-      <thead>
-        <tr>
-          <th
-            className={css`
-              min-width: 150px;
-              vertical-align: middle;
-              padding: 10px 10px 10px 0;
-              text-align: left;
-              border-bottom: 1px solid #0b1424;
-              position: sticky;
-              top: 0px;
-              left: 0;
-              z-index: 20;
-              background-color: #ffffff;
-            `}
-          >
-            Th1
-          </th>
-          <th
-            className={css`
-              vertical-align: middle;
-              padding: 10px;
-              border-bottom: 1px solid #0b1424;
-              position: sticky;
-              top: 0;
-              z-index: 10;
-              background-color: #ffffff;
-            `}
-          >
-            Th2
-          </th>
-          <th
-            className={css`
-              vertical-align: middle;
-              padding: 10px;
-              border-bottom: 1px solid #0b1424;
-              position: sticky;
-              top: 0;
-              z-index: 10;
-              background-color: #ffffff;
-            `}
-          >
-            Th3
-          </th>
-          <th
-            className={css`
-              vertical-align: middle;
-              padding: 10px;
-              border-bottom: 1px solid #0b1424;
-              position: sticky;
-              top: 0;
-              z-index: 10;
-              background-color: #ffffff;
-            `}
-          >
-            Th4
-          </th>
-          <th
-            className={css`
-              vertical-align: middle;
-              padding: 10px;
-              border-bottom: 1px solid #0b1424;
-              position: sticky;
-              top: 0;
-              z-index: 10;
-              background-color: #ffffff;
-            `}
-          >
-            Th5
-          </th>
-          <th
-            className={css`
-              vertical-align: middle;
-              padding: 10px;
-              border-bottom: 1px solid #0b1424;
-              position: sticky;
-              top: 0;
-              z-index: 10;
-              background-color: #ffffff;
-            `}
-          >
-            Th6
-          </th>
-        </tr>
-      </thead>
-      <tbody>
-        {results.map((r) => {
-          return (
-            <tr
-              key={r.key}
-              {...r.events}
+      <table
+        className={css`
+          border-collapse: separate;
+          border-spacing: 0px 0px;
+          width: 100%;
+        `}
+      >
+        <thead>
+          <tr>
+            <th
               className={css`
-                cursor: grab;
-                position: relative;
-                touch-action: none;
+                min-width: 150px;
+                vertical-align: middle;
+                padding: 10px 10px 10px 0;
+                text-align: left;
+                border-bottom: 1px solid #0b1424;
+                position: sticky;
+                top: 0px;
+                left: 0;
+                z-index: 20;
+                background-color: #ffffff;
               `}
             >
-              <th
+              Th1
+            </th>
+            <th
+              className={css`
+                vertical-align: middle;
+                padding: 10px;
+                border-bottom: 1px solid #0b1424;
+                position: sticky;
+                top: 0;
+                z-index: 10;
+                background-color: #ffffff;
+              `}
+            >
+              Th2
+            </th>
+            <th
+              className={css`
+                vertical-align: middle;
+                padding: 10px;
+                border-bottom: 1px solid #0b1424;
+                position: sticky;
+                top: 0;
+                z-index: 10;
+                background-color: #ffffff;
+              `}
+            >
+              Th3
+            </th>
+            <th
+              className={css`
+                vertical-align: middle;
+                padding: 10px;
+                border-bottom: 1px solid #0b1424;
+                position: sticky;
+                top: 0;
+                z-index: 10;
+                background-color: #ffffff;
+              `}
+            >
+              Th4
+            </th>
+            <th
+              className={css`
+                vertical-align: middle;
+                padding: 10px;
+                border-bottom: 1px solid #0b1424;
+                position: sticky;
+                top: 0;
+                z-index: 10;
+                background-color: #ffffff;
+              `}
+            >
+              Th5
+            </th>
+            <th
+              className={css`
+                vertical-align: middle;
+                padding: 10px;
+                border-bottom: 1px solid #0b1424;
+                position: sticky;
+                top: 0;
+                z-index: 10;
+                background-color: #ffffff;
+              `}
+            >
+              Th6
+            </th>
+          </tr>
+        </thead>
+        <tbody>
+          {results.map((r) => {
+            return (
+              <tr
+                key={r.key}
+                {...r.events}
                 className={css`
-                  min-width: 130px;
-                  vertical-align: middle;
-                  padding: 10px 10px 10px 0;
-                  text-align: left;
-                  font-weight: 600;
-                  user-select: none;
-                  box-sizing: border-box;
-                  border-bottom: 1px solid rgb(203 213 225);
-                  position: sticky;
-                  left: 0;
-                  box-shadow: 3px 0px 2px rgba(0, 0, 0, 0.1);
-                  background-color: #ffffff;
+                  cursor: grab;
+                  position: relative;
+                  touch-action: none;
                 `}
               >
-                {r.value}
-              </th>
-              <td
-                className={css`
-                  padding: 10px;
-                  vertical-align: middle;
-                  background-color: #ffffff;
-                  padding: 10px;
-                  text-align: center;
-                  font-weight: 600;
-                  user-select: none;
-                  box-sizing: border-box;
-                  border-bottom: 1px solid rgb(203 213 225);
-                  &:not(:last-child) {
-                    border-right: 1px solid rgb(203 213 225);
-                  }
-                `}
-              >
-                td 1
-              </td>
-              <td
-                className={css`
-                  padding: 10px;
-                  vertical-align: middle;
-                  background-color: #ffffff;
-                  padding: 10px;
-                  text-align: center;
-                  font-weight: 600;
-                  user-select: none;
-                  box-sizing: border-box;
-                  border-bottom: 1px solid rgb(203 213 225);
-                  &:not(:last-child) {
-                    border-right: 1px solid rgb(203 213 225);
-                  }
-                `}
-              >
-                td 2
-              </td>
-              <td
-                className={css`
-                  padding: 10px;
-                  vertical-align: middle;
-                  background-color: #ffffff;
-                  padding: 10px;
-                  text-align: center;
-                  font-weight: 600;
-                  user-select: none;
-                  box-sizing: border-box;
-                  border-bottom: 1px solid rgb(203 213 225);
-                  &:not(:last-child) {
-                    border-right: 1px solid rgb(203 213 225);
-                  }
-                `}
-              >
-                td 3
-              </td>
-              <td
-                className={css`
-                  padding: 10px;
-                  vertical-align: middle;
-                  background-color: #ffffff;
-                  padding: 10px;
-                  text-align: center;
-                  font-weight: 600;
-                  user-select: none;
-                  box-sizing: border-box;
-                  border-bottom: 1px solid rgb(203 213 225);
-                  &:not(:last-child) {
-                    border-right: 1px solid rgb(203 213 225);
-                  }
-                `}
-              >
-                td 4
-              </td>
-              <td
-                className={css`
-                  padding: 10px;
-                  vertical-align: middle;
-                  background-color: #ffffff;
-                  padding: 10px;
-                  text-align: center;
-                  font-weight: 600;
-                  user-select: none;
-                  box-sizing: border-box;
-                  border-bottom: 1px solid rgb(203 213 225);
-                  &:not(:last-child) {
-                    border-right: 1px solid rgb(203 213 225);
-                  }
-                `}
-              >
-                td 5
-              </td>
-            </tr>
-          );
-        })}
-      </tbody>
-    </table>
+                <th
+                  className={css`
+                    min-width: 130px;
+                    vertical-align: middle;
+                    padding: 10px 10px 10px 0;
+                    text-align: left;
+                    font-weight: 600;
+                    user-select: none;
+                    box-sizing: border-box;
+                    border-bottom: 1px solid rgb(203 213 225);
+                    position: sticky;
+                    left: 0;
+                    box-shadow: 3px 0px 2px rgba(0, 0, 0, 0.1);
+                    background-color: #ffffff;
+                  `}
+                >
+                  {r.value}
+                </th>
+                <td
+                  className={css`
+                    padding: 10px;
+                    vertical-align: middle;
+                    background-color: #ffffff;
+                    padding: 10px;
+                    text-align: center;
+                    font-weight: 600;
+                    user-select: none;
+                    box-sizing: border-box;
+                    border-bottom: 1px solid rgb(203 213 225);
+                    &:not(:last-child) {
+                      border-right: 1px solid rgb(203 213 225);
+                    }
+                  `}
+                >
+                  td 1
+                </td>
+                <td
+                  className={css`
+                    padding: 10px;
+                    vertical-align: middle;
+                    background-color: #ffffff;
+                    padding: 10px;
+                    text-align: center;
+                    font-weight: 600;
+                    user-select: none;
+                    box-sizing: border-box;
+                    border-bottom: 1px solid rgb(203 213 225);
+                    &:not(:last-child) {
+                      border-right: 1px solid rgb(203 213 225);
+                    }
+                  `}
+                >
+                  td 2
+                </td>
+                <td
+                  className={css`
+                    padding: 10px;
+                    vertical-align: middle;
+                    background-color: #ffffff;
+                    padding: 10px;
+                    text-align: center;
+                    font-weight: 600;
+                    user-select: none;
+                    box-sizing: border-box;
+                    border-bottom: 1px solid rgb(203 213 225);
+                    &:not(:last-child) {
+                      border-right: 1px solid rgb(203 213 225);
+                    }
+                  `}
+                >
+                  td 3
+                </td>
+                <td
+                  className={css`
+                    padding: 10px;
+                    vertical-align: middle;
+                    background-color: #ffffff;
+                    padding: 10px;
+                    text-align: center;
+                    font-weight: 600;
+                    user-select: none;
+                    box-sizing: border-box;
+                    border-bottom: 1px solid rgb(203 213 225);
+                    &:not(:last-child) {
+                      border-right: 1px solid rgb(203 213 225);
+                    }
+                  `}
+                >
+                  td 4
+                </td>
+                <td
+                  className={css`
+                    padding: 10px;
+                    vertical-align: middle;
+                    background-color: #ffffff;
+                    padding: 10px;
+                    text-align: center;
+                    font-weight: 600;
+                    user-select: none;
+                    box-sizing: border-box;
+                    border-bottom: 1px solid rgb(203 213 225);
+                    &:not(:last-child) {
+                      border-right: 1px solid rgb(203 213 225);
+                    }
+                  `}
+                >
+                  td 5
+                </td>
+              </tr>
+            );
+          })}
+        </tbody>
+      </table>
+    </div>
   );
 }
 
