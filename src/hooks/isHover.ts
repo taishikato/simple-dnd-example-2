4eaf9913a42370c1c924e7d948de63a2aa939a60
taishikato <taishi.k0903@gmail.com>
2022-08-05 11:35:20 -0700

    update isHover logic

--- src/hooks/isHover.ts
+++ src/hooks/isHover.ts
@@ -1,15 +1,9 @@
 export const isHover = (event: PointerEvent, element: HTMLElement): boolean => {
   // マウスポインターの座標を取得
-  const clientX = event.clientX;
   const clientY = event.clientY;
 
   // 重なりを判定する要素のサイズと座標を取得
   const rect = element.getBoundingClientRect();
 
-  return (
-    clientY < rect.bottom &&
-    clientY > rect.top &&
-    clientX < rect.right &&
-    clientX > rect.left
-  );
+  return clientY < rect.bottom && clientY > rect.top;
 };
