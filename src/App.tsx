8b255eeb89d46909a8a8356ef8e07392d4e00446
taishikato <taishi.k0903@gmail.com>
2022-08-05 15:58:12 -0700

    delete itemList variable

--- src/App.tsx
+++ src/App.tsx
@@ -4,41 +4,6 @@ import type { ColumnProps, DataProps } from "./types";
 import moment from "moment";
 import "moment-timezone";
 
-const itemList = [
-  { header: "Header 1", value: "Hello" },
-  {
-    header: "header 2",
-    value: (
-      <img
-        src="https://hips.hearstapps.com/hmg-prod.s3.amazonaws.com/images/bbyoda-1575303784.jpeg?resize=480:*"
-        className={css`
-          border-radius: 10px;
-        `}
-      />
-    ),
-  },
-  { header: "Header 3", value: "Hola" },
-  {
-    header: "Header 4",
-    value: (
-      <div
-        className={css`
-          padding: 20px;
-          background-color: green;
-          border-radius: 10px;
-          color: white;
-        `}
-      >
-        div element
-      </div>
-    ),
-  },
-  {
-    header: "Header 5",
-    value: 47247318,
-  },
-];
-
 export type BanyanValueType =
   | "temperature_c_above_max"
   | "temperature_c_in_max"
